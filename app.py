2e0c3b9720b4b7941436a4bf5d70a41376bb3cb7
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-06-25 20:11:50 +0200

    New pages

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
