393567371738843b0bd28d2ebbb4f039f0aaedc2
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-06-27 00:26:01 +0200

    Restart button

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from model import Game
 
 app = Flask(__name__)
 game = Game()
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', cards=game.deck.cards, rows=4, columns=3)
+    if request.method == "POST":
+        if request.form.get("restart"):
+            game.restart()
+            return redirect("/")
+    else:
+        return render_template('index.html', cards=game.deck.cards, rows=4, columns=3)
 
 @app.route('/leaderboard')
 def leaderboard():
